5b8d60dcd55e5706078857d165942c067d5021e5
Aarjav Jain <arjavjainofficial@gmail.com>
2026-07-20 22:59:00 +0530

    Complete Motor module implementation

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -52,3 +52,27 @@ void moveBackward()
     digitalWrite(IN3, LOW); 
     digitalWrite(IN4, HIGH);
 }
+
+void turnLeft()
+{
+    analogWrite(ENA, 255); 
+    analogWrite(ENB, 255); 
+    
+    digitalWrite(IN1, LOW); 
+    digitalWrite(IN2, HIGH); 
+    
+    digitalWrite(IN3, HIGH); 
+    digitalWrite(IN4, LOW);
+}
+
+void turnRight()
+{
+    analogWrite(ENA, 255); 
+    analogWrite(ENB, 255); 
+    
+    digitalWrite(IN1, HIGH); 
+    digitalWrite(IN2, LOW); 
+    
+    digitalWrite(IN3, LOW); 
+    digitalWrite(IN4, HIGH);
+}
