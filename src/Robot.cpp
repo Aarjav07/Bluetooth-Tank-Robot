68e404d274b57e985ae66e4c51eb1baf765b2a01
Aarjav Jain <arjavjainofficial@gmail.com>
2026-07-21 16:47:09 +0530

    feat: implement robot state machine

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -4,6 +4,13 @@
 #include "Motor.h"
 #include "Config.h"
 
+static RobotState currentState = RobotState::STOPPED;
+
+RobotState getRobotState()
+{
+    return currentState;
+}
+
 void robotInit()
 {
     Serial.println("[Robot] Initialized");
@@ -14,26 +21,58 @@ void processCommand(char command)
     switch(command)
     {
         case CMD_FORWARD:
+            if (currentState != RobotState::FORWARD)
+            {
             moveForward();
+            currentState = RobotState::FORWARD;
+
+            Serial.println("[Robot] Moving Forward");
+            }
             break;
 
         case CMD_BACKWARD:
+            if (currentState != RobotState::BACKWARD)
+            {
             moveBackward();
+            currentState = RobotState::BACKWARD;
+
+            Serial.println("[Robot] Moving Backward");
+            }
             break;
 
         case CMD_LEFT:
+            if (currentState != RobotState::LEFT)
+            {
             turnLeft();
+            currentState = RobotState::LEFT;
+
+            Serial.println("[Robot] Turning Left");
+            }
             break;
 
         case CMD_RIGHT:
+            if (currentState != RobotState::RIGHT)
+            {
             turnRight();
+            currentState = RobotState::RIGHT;
+
+            Serial.println("[Robot] Turning Right");
+            }
             break;
 
         case CMD_STOP:
+            if (currentState != RobotState::STOPPED)
+            {
             stopMotors();
+            currentState = RobotState::STOPPED;
+
+            Serial.println("[Robot] Stopped");
+            }
             break;
 
         default:
+            Serial.print("[Robot] Unknown command: ");
+            Serial.println(command);
             break;
     }
 }
--- src/Robot.h
+++ src/Robot.h
@@ -1,8 +1,19 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
+enum class RobotState
+{
+    STOPPED,
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
 void robotInit();
 
 void processCommand(char command);
+
+RobotState getRobotState();
 
 #endif
